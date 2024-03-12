66a5f47e13963450179caf551b6bf45b80cb45fc
WAHJADAI <drafzaza123@icloud.com>
2024-03-12 09:46:36 +0700

    edit show category

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
                   <div className="text-sm font-medium text-gray-900">
-                    {post.category}
+                    {post.category.name}
                   </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
